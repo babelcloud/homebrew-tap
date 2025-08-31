030a3d571dab420ee0f14b1722caaa4897e1cab3
robot <robot@babel.cloud>
2025-08-31 08:57:53 +0000

    Publish v0.3.5

--- vzbox.rb
+++ vzbox.rb
@@ -3,10 +3,10 @@ require_relative "private_strategy"
 class Vzbox < Formula
   desc "vzbox"
   homepage "https://github.com/babelcloud/vzbox"
-  version "v0.3.4"
+  version "v0.3.5"
   url "https://github.com/babelcloud/vzbox/releases/download/#{version}/vzbox",
       using: GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "71fa0dd41ed8da69bbdb18c9d514ffc325c688eaf58d19a44f1585685a42edb2"
+  sha256 "3181188785ba86f0adfbe6c53a281a0fa0f052c2fa337c94734afc883535314e"
 
   depends_on "qemu"
 
